4cff7227bddb8086e71f3436fdf3d894ee55a123
Mateusz Neumann <mateumann@gmail.com>
2019-11-12 12:35:07 +0100

    Can read TRex tcpdumps
    Read and parse tcpdumps generated during TRex traffic is running.
    Distinguishes micro- and nanoseconds pcaps.  Parses timestamps and
    calculates latencies.  Generates a slice of TrexPcapStats that can
    be used to generate some visualizations.

--- main.go
+++ main.go
@@ -1,38 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"time"
 
-import "github.com/google/gopacket"
-import "github.com/google/gopacket/pcap"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+)
 
-func parsePcap(filename string) (count uint32, err error) {
-    if handle, err := pcap.OpenOffline(filename); err != nil {
-        return 0, err
-    } else {
-        fmt.Printf("Opening file with %s timestamp resolution.\n", handle.Resolution())
-        packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-        count = 0
-        for packet := range packetSource.Packets() {
-            handlePacket(packet) // Do something with a packet here.
-            count++
-        }
-    }
-    return count, nil
+type TrexPcapStat struct {
+	timestamp time.Time
+	genre     uint8
+	latency   int64
 }
 
-func handlePacket(packet gopacket.Packet) {
-    //packet.
+func parsePcap(filename string) (stats []TrexPcapStat, err error) {
+	var handle *pcap.Handle
+	var stat TrexPcapStat
+	if handle, err = pcap.OpenOffline(filename); err != nil {
+		return make([]TrexPcapStat, 0), err
+	}
+	fmt.Printf("Opening file %s with %s handler.\n", filename, handle.LinkType())
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	notParsedPackets := 0
+	for packet := range packetSource.Packets() {
+		if stat, err = handlePacket(packet); err != nil {
+			notParsedPackets++
+		} else {
+			stats = append(stats, stat)
+		}
+	}
+	fmt.Printf("Parsing finished, skipped %d packets.\n", notParsedPackets)
+	return stats, nil
+}
+
+func handlePacket(packet gopacket.Packet) (stat TrexPcapStat, err error) {
+	var ipLayer gopacket.Layer
+	if ipLayer = packet.Layer(layers.LayerTypeIPv4); ipLayer == nil {
+		return stat, errors.New("not an IPv4 packet")
+	}
+	stat.timestamp = packet.Metadata().Timestamp
+	switch stat.genre = ipLayer.LayerPayload()[14]; stat.genre {
+	case 0xab:
+		ns := int64(binary.LittleEndian.Uint64(ipLayer.LayerPayload()[22:30]))
+		stat.latency = stat.timestamp.UnixNano() - ns
+	case 0x58:
+		stat.latency = 0
+	default:
+		return stat, errors.New("not interesting packet")
+	}
+	return stat, nil
 }
 
 func main() {
-    filename := "/home/mateusz/Projects/trex-helpers/data/001-br-15397ec68cd0.pcap"
-    //filename := "/home/mateusz/Projects/trex-helpers/data/001-br-51d85eb72d54.pcap"
-    //filename := "/home/mateusz/Projects/trex-helpers/data/001-enp3s0.pcap"
-    fmt.Println("Hello world")
-    packets, err := parsePcap(filename)
-    if err != nil {
-        fmt.Printf("File %s could not be read", filename)
-    } else {
-        fmt.Printf("File %s parsed; %d packets read", filename, packets)
-    }
+	//filename := "/home/mateusz/Projects/trex-helpers/data/001-br-15397ec68cd0.pcap"
+	//filename := "/home/mateusz/Projects/trex-helpers/data/001-br-51d85eb72d54.pcap"
+	//filename := "/home/mateusz/Projects/trex-helpers/data/002-br-571f91c727a3.pcap"
+	//filename := "/home/mateusz/Projects/trex-helpers/data/002-br-e0e2570b862d.pcap"
+	//filename := "/home/mateusz/Projects/trex-helpers/data/003-br-468f95065bbe.pcap"
+	filename := "/home/mateusz/Projects/trex-helpers/data/003-br-8f3178dc1ac3.pcap"
+	packets, err := parsePcap(filename)
+	if err != nil {
+		fmt.Printf("File %s could not be read", filename)
+	} else {
+		fmt.Printf("File %s parsed; %d packets read", filename, len(packets))
+	}
 }
