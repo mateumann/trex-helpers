4b13bf228c673a88585adb804afd8fd75a98bb34
Mateusz Neumann <mateumann@gmail.com>
2019-11-14 23:30:57 +0100

    Skeleton for the second implementation (scattered)

---
+++ cmd/latency-plot/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+
+	"path/filepath"
+)
+
+func main() {
+	debug := false
+
+	if os.Getenv("DEBUG") != "" {
+		debug = true
+	}
+
+	if debug {
+		fmt.Printf("Running %v\n", filepath.Base(os.Args[0]))
+	}
+}
