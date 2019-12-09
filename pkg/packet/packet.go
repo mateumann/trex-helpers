46606fcc24f630c7698612582dfaf60885b52963
Mateusz Neumann <mateumann@gmail.com>
2019-12-09 15:26:46 +0100

    [WIP] 2019-12-10 at work

--- pkg/packet/packet.go
+++ pkg/packet/packet.go
@@ -4,6 +4,7 @@ import (
 	"encoding/binary"
 	"errors"
 	"fmt"
+	"math"
 	"time"
 
 	"github.com/google/gopacket"
@@ -11,54 +12,122 @@ import (
 	"github.com/google/gopacket/pcap"
 )
 
-type TRexLatencyStat struct {
-	receivedTimestamp time.Time
-	seq               uint32
-	latency           time.Duration
+type Type uint8
+
+const (
+	TypeLatency Type = 0xab
+	TypePTP     Type = 0xf7
+	TypeOther   Type = 0xff
+)
+
+const InvalidLatency time.Duration = -1
+
+type Packet interface {
+	ReceivedAt() time.Time
+	Type() Type
+	Value() float64
 }
 
-type trexLatencyPkt struct {
+type LatencyPacket struct {
 	received time.Time
-	flowSeq  uint8
-	hwID     uint16
-	seq      uint32
-	ts       time.Time
+	latency  time.Duration
 }
 
-func ParsePcap(filename string, verbose bool) (stats []TRexLatencyStat, err error) {
+func (pkt LatencyPacket) ReceivedAt() time.Time {
+	return pkt.received
+}
+
+func (pkt LatencyPacket) Type() Type {
+	return TypeLatency
+}
+
+func (pkt LatencyPacket) Value() float64 {
+	// return latency in µs
+	return float64(pkt.latency) / 1000
+}
+
+type PTPPacket struct {
+	received  time.Time
+	messageID uint8
+}
+
+func (pkt PTPPacket) ReceivedAt() time.Time {
+	return pkt.received
+}
+
+func (pkt PTPPacket) Type() Type {
+	return TypePTP
+}
+
+func (pkt PTPPacket) Value() float64 {
+	// kind of 0.2 µs high peak
+	return 200.0
+}
+
+type OtherPacket struct {
+	received time.Time
+}
+
+func (pkt OtherPacket) ReceivedAt() time.Time {
+	return pkt.received
+}
+
+func (pkt OtherPacket) Type() Type {
+	return TypeOther
+}
+
+func (pkt OtherPacket) Value() float64 {
+	return 10.0
+}
+
+func ParsePcap(filename string, verbose bool) (packets []Packet, err error) {
 	if verbose {
 		fmt.Printf("Parsing %v file... ", filename)
 	}
 	var handle *pcap.Handle
 	if handle, err = pcap.OpenOffline(filename); err != nil {
-		return stats, err
+		return packets, err
 	}
 	defer handle.Close()
 
-	totalPackets, latencyPackets := 0, 0
+	totalPackets, latencyPackets, invalidLatencyPackets, ptpPackets := 0, 0, 0, 0
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
 		totalPackets++
 		packet := packet // pin!
-		pkt, err := handleLatencyPacket(packet)
+		received := packet.Metadata().Timestamp.UTC()
+		latencyPkt, err := handleLatencyPacket(packet, received)
 		if err == nil {
 			latencyPackets++
-			s := TRexLatencyStat{
-				receivedTimestamp: pkt.received,
-				seq:               pkt.seq,
-				latency:           pkt.received.Sub(pkt.ts),
+			packets = append(packets, latencyPkt)
+			if latencyPkt.latency == InvalidLatency {
+				invalidLatencyPackets++
 			}
-			stats = append(stats, s)
+			continue
 		}
+		ptpPkt, err := handlePTPPacket(packet, received)
+		if err == nil {
+			ptpPackets++
+			packets = append(packets, ptpPkt)
+			continue
+		}
+		ptpInIPPkt, err := handlePTPinIPPacket(packet, received)
+		if err == nil {
+			ptpPackets++
+			packets = append(packets, ptpInIPPkt)
+			continue
+		}
+		packets = append(packets, OtherPacket{received})
 	}
 	if verbose {
-		fmt.Printf("got %v packets including %v latency packets\n", totalPackets, latencyPackets)
+		fmt.Printf("got %v packets including %v latency and %v PTP packets; %v latency packets are somewhat invalid\n",
+			totalPackets, latencyPackets, ptpPackets, invalidLatencyPackets)
 	}
-	return stats, nil
+	return packets, nil
 }
 
-func handleLatencyPacket(packet gopacket.Packet) (trexLatencyPkt, error) {
-	var result trexLatencyPkt
+func handleLatencyPacket(packet gopacket.Packet, received time.Time) (LatencyPacket, error) {
+	var result LatencyPacket
 	ipLayer := packet.Layer(layers.LayerTypeIPv4)
 	if ipLayer == nil {
 		return result, errors.New("not a IPv4 packet")
@@ -66,15 +135,52 @@ func handleLatencyPacket(packet gopacket.Packet) (trexLatencyPkt, error) {
 	if len(ipLayer.LayerPayload()) < 16 {
 		return result, errors.New("not valid TRex Latency packet")
 	}
-	trexPayload := ipLayer.LayerPayload()[len(ipLayer.LayerPayload())-16:]
-	if trexPayload[0] != 0xab {
+	latencyData := ipLayer.LayerPayload()[len(ipLayer.LayerPayload())-16:]
+	if latencyData[0] != 0xab {
 		return result, errors.New("not valid TRex Latency packet")
 	}
-	result.received = packet.Metadata().Timestamp.UTC()
-	result.flowSeq = trexPayload[1]
-	result.hwID = binary.LittleEndian.Uint16(trexPayload[2:4])
-	result.seq = binary.LittleEndian.Uint32(trexPayload[4:8])
-	timestamp := binary.LittleEndian.Uint64(trexPayload[8:16])
-	result.ts = time.Unix(int64(timestamp/1000000000), int64(timestamp%1000000000)).UTC()
+
+	result.received = received
+	timestamp := binary.LittleEndian.Uint64(latencyData[8:16])
+	sent := time.Unix(int64(timestamp/1000000000), int64(timestamp%1000000000)).UTC()
+	result.latency = received.Sub(sent)
+	if math.Abs(float64(result.latency)) > float64(time.Second)*10 {
+		result.latency = InvalidLatency
+	}
 	return result, nil
+}
+
+func handlePTPPacket(packet gopacket.Packet, received time.Time) (PTPPacket, error) {
+	var result PTPPacket
+	ethLayer := packet.Layer(layers.LayerTypeEthernet)
+	if ethLayer == nil {
+		return result, errors.New("not an Ethernet packet")
+	}
+
+	return parsePTPPayload(ethLayer.LayerPayload(), received)
+}
+
+func handlePTPinIPPacket(packet gopacket.Packet, received time.Time) (PTPPacket, error) {
+	var result PTPPacket
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer == nil {
+		return result, errors.New("not a IPv4 packet")
+	}
+	udpLayer := packet.Layer(layers.LayerTypeUDP)
+	if udpLayer == nil {
+		return result, errors.New("not a UDP packet")
+	}
+
+	return parsePTPPayload(udpLayer.LayerPayload(), received)
+}
+
+func parsePTPPayload(data []byte, received time.Time) (result PTPPacket, err error) {
+	if ((data[0] == 0x00 || data[0] == 0x01 || data[0] == 0x08) && len(data) == 44) ||
+		(data[0] == 0x09 && len(data) == 54) {
+		result.received = received
+		result.messageID = data[0]
+		return result, nil
+	}
+
+	return result, errors.New("not a PTP packet")
 }
--- pkg/packet/packet_test.go
+++ pkg/packet/packet_test.go
@@ -14,23 +14,37 @@ func TestParsePcap(t *testing.T) {
 	tests := []struct {
 		name      string
 		args      args
-		wantStats []TRexLatencyStat
+		wantStats []Packet
 		wantErr   bool
 	}{
 		{"Return error for empty file", args{"", false}, nil, true},
 
 		{"Single latency packet", args{"../../tests/001-one-latency-pkt.pcap", false},
-			[]TRexLatencyStat{
-				{time.Date(2019, 12, 05, 15, 17, 48, 959001355, time.Local).UTC(),
-					50, 243065},
+			[]Packet{
+				LatencyPacket{time.Date(2019, 12, 05, 15, 17, 48, 959001355, time.Local).UTC(),
+					243065},
 			}, false},
 
 		{"Mixed twenty packets", args{"../../tests/002-mixed-twenty-ptp-latency-other.pcap", false},
-			[]TRexLatencyStat{
-				{time.Date(2019, 12, 9, 9, 4, 7, 213015280, time.Local).UTC(),
-					0x50, -10509},
-				{time.Date(2019, 12, 9, 9, 4, 7, 262985012, time.Local).UTC(),
-					0x51, -15250},
+			[]Packet{
+				OtherPacket{time.Date(2019, 12, 9, 9, 4, 7, 121075768, time.Local).UTC()},
+				OtherPacket{time.Date(2019, 12, 9, 9, 4, 7, 121109985, time.Local).UTC()},
+				PTPPacket{time.Date(2019, 12, 9, 9, 4, 7, 121610958, time.Local).UTC(), 0x00},
+				OtherPacket{time.Date(2019, 12, 9, 9, 4, 7, 122082686, time.Local).UTC()},
+				OtherPacket{time.Date(2019, 12, 9, 9, 4, 7, 122125297, time.Local).UTC()},
+				PTPPacket{time.Date(2019, 12, 9, 9, 4, 7, 122583224, time.Local).UTC(), 0x08},
+				OtherPacket{time.Date(2019, 12, 9, 9, 4, 7, 123075232, time.Local).UTC()},
+				OtherPacket{time.Date(2019, 12, 9, 9, 4, 7, 123152834, time.Local).UTC()},
+				PTPPacket{time.Date(2019, 12, 9, 9, 4, 7, 123193888, time.Local).UTC(), 0x01},
+				PTPPacket{time.Date(2019, 12, 9, 9, 4, 7, 123569061, time.Local).UTC(), 0x09},
+				OtherPacket{time.Date(2019, 12, 9, 9, 4, 7, 124150750, time.Local).UTC()},
+				LatencyPacket{time.Date(2019, 12, 9, 9, 4, 7, 213015280, time.Local).UTC(),
+					-10509},
+				OtherPacket{time.Date(2019, 12, 9, 9, 4, 7, 213098093, time.Local).UTC()},
+				OtherPacket{time.Date(2019, 12, 9, 9, 4, 7, 213133027, time.Local).UTC()},
+				OtherPacket{time.Date(2019, 12, 9, 9, 4, 7, 213987195, time.Local).UTC()},
+				LatencyPacket{time.Date(2019, 12, 9, 9, 4, 7, 262985012, time.Local).UTC(),
+					-15250},
 			}, false},
 	}
 
